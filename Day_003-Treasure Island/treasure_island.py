ca741ed8669e5abcd3c5cb1d0e20817c71368ed8
Iseries2090 <khayden2090@gmail.com>
2021-07-29 08:18:39 -0500

    Completed Day 3 project

--- Day_003-Treasure Island/treasure_island.py
+++ Day_003-Treasure Island/treasure_island.py
@@ -22,4 +22,21 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
+choice = input('''You're at a cross road. Where do you want to go? Type "left or "right"''')
+if choice.lower() == "left":
+    choice = input('''You come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a 
+    boat. Type "swim" to swim across.''')
+    if choice.lower() == "wait":
+        choice = input('''You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow, 
+        and one blue. Which colour do you choose?''')
+        if choice.lower() == "yellow":
+            print("You Win!")
+        elif choice.lower() == "blue":
+            print("Eaten by beasts. Game Over.")
+        else:
+            print("Game over.")
+    else:
+        print("Attacked by trout. Game Over.")
+else:
+    print("You fall into a hole. Game over.")
 
