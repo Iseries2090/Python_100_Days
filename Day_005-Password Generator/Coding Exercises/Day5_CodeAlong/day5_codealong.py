366a686e5bf8e069231fa285d052534952505f8c
Iseries2090 <khayden2090@gmail.com>
2021-08-01 16:46:19 -0500

    Completed Day 5 project

---
+++ Day_005-Password Generator/Coding Exercises/Day5_CodeAlong/day5_codealong.py
@@ -0,0 +1,15 @@
+# fruits = ["apple", "peach", "pear"]
+#
+# for fruit in fruits:
+#     print(fruit)
+
+
+#The Range Function
+# for number in range (a,b):
+#     print(number)
+for number in range(1, 11):
+    print(number)
+total = 0
+for number in range(1,101):
+    total += number
+print(total)
