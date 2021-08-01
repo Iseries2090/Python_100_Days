4516077210ebe45ae96e5c3b75167b0e37127ef0
Iseries2090 <khayden2090@gmail.com>
2021-08-01 09:56:04 -0500

    Completed Day 4-3 exercise

---
+++ Day_004-RockPaperScissors/Coding Exercises/Day4_CodeAlong/Day4_CodeAlong.py
@@ -0,0 +1,11 @@
+#This is just a code-along with the videos
+
+import random
+
+
+# randon_integer = random.randint(1,10)
+# print(randon_integer)
+
+fruits = ["apple", "banana", "pear"]
+
+print(fruits)
