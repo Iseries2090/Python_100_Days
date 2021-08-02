dd0c45f5f00c249d472f541492356cd025c7d1c3
Iseries2090 <khayden2090@gmail.com>
2021-08-02 07:27:52 -0500

    Completed Day 6

---
+++ Day_006-KarlTheRobot/Coding Exercises/Day6-1-Exercise/day6-1-exercise.py
@@ -0,0 +1,18 @@
+#Code along
+
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+#Creating / defining a new function
+
+def my_function():
+    print('Hello')
+    print('Goodbye')
+my_function()
+
+#defining Functions
+# def my_function():
+#     Do this
+#     then do this
+#     Finally do this
---
+++ Day_006-KarlTheRobot/karl_maze.py
@@ -0,0 +1,19 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def move_to_wall():
+    while front_is_clear():
+        move()
+
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
