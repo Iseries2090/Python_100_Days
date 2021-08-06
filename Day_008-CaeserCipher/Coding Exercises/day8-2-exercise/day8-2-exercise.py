94f5bf34c90a6f56d54e4788f6012384aca75d1c
Iseries2090 <khayden2090@gmail.com>
2021-08-06 08:30:57 -0500

    Completed Day 8-2 exercise

---
+++ Day_008-CaeserCipher/Coding Exercises/day8-2-exercise/day8-2-exercise.py
@@ -0,0 +1,19 @@
+# Write your code below this line 👇
+def prime_checker(number):
+    if number % 2 == 0:
+        print("It's not a prime number.")
+    elif number % 3 == 0:
+        print("It's not a prime number.")
+    elif number % 5 == 0:
+        print("It's not a prime number.")
+    elif number % 7 == 0:
+        print("It's not a prime number.")
+    else:
+        print("It's a prime number.")
+
+# Write your code above this line 👆
+
+# Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
+
