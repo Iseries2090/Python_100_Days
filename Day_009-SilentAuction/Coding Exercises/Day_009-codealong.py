d149874d277bb6440547b3d8bfe69ed54aea5a46
Iseries2090 <khayden2090@gmail.com>
2021-08-09 07:44:19 -0500

    Day 9 updated codealong

--- Day_009-SilentAuction/Coding Exercises/Day_009-codealong.py
+++ Day_009-SilentAuction/Coding Exercises/Day_009-codealong.py
@@ -21,7 +21,40 @@ empty_dictionary = {}
 # programming_dictionary["Bug"] = "A moth in your computer"
 # print(programming_dictionary)
 
-#loop through a dictionary
-for key in programming_dictionary:
-    print(key)
-    print(programming_dictionary[key])
+# #loop through a dictionary
+# for key in programming_dictionary:
+#     print(key)
+#     print(programming_dictionary[key])
+
+#**********NESTING****************
+
+# capitals = {
+#     "France": "Paris",
+#     "Germany": "Berlin",
+# }
+#
+# #Nesting a list in a dictionary
+# travel_log = {
+#     "France":["Paris","Lille","Dijon"],
+#     "Germany": ["Berlin","Hamburg","Stuttgart"]
+# }
+
+#Nesting Dctionary in a Dictionary
+travel_log = {
+    "France": {"cities_visited": ["Paris","Lille","Dijon"], "total_visits": 12},
+    "Germany": {"cities_visted": ["Berlin","Hamburg","Stuttgart"], "total_vists": 5}
+}
+
+#nesting Dictionary in a lists
+travel_log = [
+    {
+    "country": "France",
+    "cities_visited": ["Paris","Lille","Dijon"],
+    "total_visits": 12
+     },
+    {
+    "country": "Germany",
+    "cities_visted": ["Berlin","Hamburg","Stuttgart"],
+    "total_vists": 5
+    }
+]
