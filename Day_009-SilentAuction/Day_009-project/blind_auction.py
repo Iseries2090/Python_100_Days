f8df0716020d78438ebd07e70066bd0a966e3047
Iseries2090 <khayden2090@gmail.com>
2021-08-10 07:31:08 -0500

    Day 9 Project completed

---
+++ Day_009-SilentAuction/Day_009-project/blind_auction.py
@@ -0,0 +1,35 @@
+# import only system from os
+import os
+from art import logo
+# import sleep to show output for some time period
+from time import sleep
+
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+print(logo)
+print("Welcome to the secret auction program.")
+auction = {}
+keep_going = "yes"
+while keep_going == "yes":
+    user = input("What is your name?: ")
+    bid_amount = int(input("What's your bid?: "))
+
+    auction[user] = bid_amount
+    keep_going = input("Are there any other bidders? Type 'yes' or 'no'. \n").lower()
+
+    #Clear screen doesn't seem to work, not sure the issue
+    clear_screen()
+
+current_winner = ""
+current_highest = 0
+for key in auction:
+    if auction[key] > current_highest:
+        current_winner = key
+        current_highest = auction[key]
+
+print(f"The winner is {current_winner} with a bid of {auction[current_winner]}")
+
+
